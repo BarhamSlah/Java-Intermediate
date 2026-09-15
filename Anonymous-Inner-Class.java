533aa0ec0848fa4634eb382a2531f9be90401da6
Barham Salahaddin <barhamsalahaddin@gmail.com>
2026-09-15 14:36:38 +0300

    Create Anonymous-Inner-Class.java

---
+++ Anonymous-Inner-Class.java
@@ -0,0 +1,19 @@
+ An anonymous inner class is a class without a name that you create and use at the same time.
+ "I need a small class here, but I don't need to create a separate named class for it."
+ It's mainly designed for one-time or very specific use.
+ If you need the same implementation in many places → create a normal named class instead.
+ It can implement an interface
+ It can extend a class
+
+🔹 Rule 1: If you put a class after new, it creates an unnamed subclass (extends).
+🔹 Rule 2: If you put an interface after new, it creates an unnamed implementing class (implements). 
+
+It can never use both extends and implements at the same time.
+It can never implement more than one interface.
+An anonymous inner class can never extend more than one class.
+
+
+Everything inside those highlighted {} curly braces is the body of a anonymous inner class
+
+
+Since an anonymous inner class has no name, you can never write a constructor for it (because in Java, constructors must match the class name).
